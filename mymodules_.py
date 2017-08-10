0826d57b3836c3cb51705de27e6d1a8ee725f865
imre.lajtos@semilab.hu <imre.lajtos@semilab.hu>
2017-08-10 17:31:48 +0200

    devel

--- mymodules_.py
+++ mymodules_.py
@@ -7,22 +7,22 @@ from matplotlib import pyplot as plt
 import cv2
 from  tkinter import *
 from tkinter import filedialog
-import mymodules_ as mm
 
-# def showImageCV(im):
-    # cv2.imshow('image',im)
-    # k = cv2.waitKey(0)
-    # if k == 27:         # wait for ESC key to exit
-        # cv2.destroyAllWindows()
-    # elif k == ord('s'): # wait for 's' key to save and exit
-        # cv2.imwrite('messigray.png',im)
-        # cv2.destroyAllWindows()
+def showImageCV(im):
+    cv2.imshow('image',im)
+    k = cv2.waitKey(0)
+    if k == 27:         # wait for ESC key to exit
+        cv2.destroyAllWindows()
+    elif k == ord('s'): # wait for 's' key to save and exit
+        cv2.imwrite('messigray.png',im)
+        cv2.destroyAllWindows()
 		
-def openFileDia():
+def openFileDia(initialdir, fileType):
     root = Tk()
-    root.filename =  filedialog.askopenfilename(initialdir = "C:/",title = "choose your file",filetypes = (("image files","*.tif"),("all files","*.*")))
-    #print (root.filename)
+    root.filename =  filedialog.askopenfilename(initialdir = initialdir,title = "choose your file",filetypes = (("image files",fileType),("all files","*.*")))
     root.withdraw()
-    return root.filename
+    filePath=root.filename
+    dirPath = filePath[0:-1*len(filePath.split('/')[-1])]
+    return [filePath, dirPath]
 	
 # filePath = mm.openFileDia()
